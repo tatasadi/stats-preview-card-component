085bcd5b4b31f930342155293a5afcb8fafbd373
Ehsan Tatasadi <ehsan@tatasadi.com>
2023-12-10 13:20:55 +0100

    fix build

--- src/components/Stat.tsx
+++ src/components/Stat.tsx
@@ -2,7 +2,7 @@ import { Lexend_Deca } from "next/font/google"
 
 const lexendDeca = Lexend_Deca({ subsets: ["latin"], weight: "400" })
 
-export default function Stat({ digit, text }) {
+export default function Stat({ digit, text }: { digit: string; text: string }) {
   return (
     <div className="mb-6 lg:mr-16">
       <div className="text-2xl leading-normal font-bold">{digit}</div>
