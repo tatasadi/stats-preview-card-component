3cd908c51ebb80945cdc5a5018b5d0399c38c441
Ehsan Tatasadi <ehsan@tatasadi.com>
2023-12-10 13:27:19 +0100

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "@/components/Footer"
 import StatsPreviewCard from "@/components/StatsPreviewCard"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center px-6 py-24">
+    <main className="flex min-h-screen flex-col items-center justify-center px-6 py-24 gap-16">
       <StatsPreviewCard />
+      <Footer />
     </main>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,31 @@
+export default function Footer() {
+  return (
+    <p className="text-center text-sm text-white">
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io"
+        target="_blank"
+        className="footer-link"
+      >
+        Frontend Mentor
+      </a>
+      <br />
+      Coded by{" "}
+      <a
+        className="footer-link"
+        href="https://www.linkedin.com/in/ehsan-tatasadi-2161a433"
+        target="_blank"
+      >
+        Ehsan Tatasadi
+      </a>
+      <br />
+      <a
+        className="footer-link"
+        href="https://github.com/tatasadi/stats-preview-card-component"
+        target="_blank"
+      >
+        Source Code
+      </a>
+    </p>
+  )
+}
